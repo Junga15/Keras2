0b241850d9b0e241d8500b2bb8bd283387417f91
JungaKim <jungakim1551@gmail.com>
2021-01-06 20:00:04 +0900

    upload

--- keras15_ensemble1.py
+++ keras15_ensemble1.py
@@ -1,45 +1,79 @@
-#10_3 데이터끌어온것임 #다대다 앙상블
-#실습.위 두개의 모델을 퉁칠 수 있는 RMSE와 R2값을 구하라.(위의 print(loss)=두개통합 로스값처럼)
-#=> 앙상블 모델의 RMSE,R2
+#keras15_ensemble1.py 실습
+#Input이 2개,Output이 2개, 즉 데이터 구성시 x값이 2개,y값이 2개
+# 다대다(2:2)앙상블 모델구현 (※10_3모델 다대다 다층퍼셉트론 모델 가져옴)
+#두개 모두 덴스형 함수형 모델로 구현했음
+#실습.위 두개의 모델을 합한 RMSE와 R2값도 구하라.(위의 print(loss)=두개통합 로스값처럼)
 
+'''
+====================
+[3504.869873046875, 2336.531982421875, 1168.338134765625, 41.61107635498047, 27.34322738647461]
+model.metrics_names: ['loss', 'dense_12_loss', 'dense_16_loss', 'dense_12_mae', 'dense_16_mae']
+====================
+y1_predict:
+ [[800.5207    21.019638 242.76799 ]
+ [801.6176    21.0073   243.07729 ]
+..... 
+ [820.26624   20.79716  248.33562 ]
+ [821.3632    20.784803 248.64497 ]]
+====================
+y2_predict:
+ [[566.48883  799.6332    33.210896]
+ [567.2308   800.7153    33.27792 ]
+ ......
+ [579.1021   818.03033   34.348827]
+ [579.84393  819.1125    34.415787]
+ [580.58594  820.19464   34.48279 ]]
+RMSE1: 48.337691009237446
+RMSE2: 34.180955395563515
+RMSE: 41.25932320240048
+r2_1: -69.27165028885757
+r2_2: -34.13797629333842
+r2: -51.70481329109799
+'''
+
+#1.데이터 구성
 import numpy as np
 
-x1=np.array([range(100),range(301,401),range(1,101)]) #print(x1.shape)는 (3,100)
-y1=np.array([range(711,811),range(1,101),range(201,301)])
+x1=np.array([range(100),range(301,401),range(1,101)]) 
+#print(x1.shape)=(3,100)
+y1=np.array([range(711,811),range(1,101),range(201,301)])  
+#print(y1.shape)=(3,100)
 
+x2=np.array([range(101,201),range(411,511),range(100,200)]) 
+#print(x2.shape)=(3,100)
+y2=np.array([range(501,601),range(711,811),range(100)])  
+#print(y2.shape)=(3,100)
 
-x2=np.array([range(101,201),range(411,511),range(100,200)])
-y2=np.array([range(501,601),range(711,811),range(100)])
+x1=np.transpose(x1) #(100,3)
+x2=np.transpose(x2) #(100,3)
 
-x1=np.transpose(x1) #4개 모두 (100,3)
-x2=np.transpose(x2)
-y1=np.transpose(y1)
-y2=np.transpose(y2)
+y1=np.transpose(y1) #(100,3)
+y2=np.transpose(y2) #(100,3)
 
+#x1,2,y1,2트레인과 테스트 분리해줌
 from sklearn.model_selection import train_test_split
 x1_train,x1_test,y1_train,y1_test=train_test_split(x1,y1,shuffle=False,train_size=0.8)
 
 from sklearn.model_selection import train_test_split
 x2_train,x2_test,y2_train,y2_test=train_test_split(x2,y2,shuffle=False,train_size=0.8)
 
-
 #2. 모델구성
 from tensorflow.keras.models import Sequential,Model
 from tensorflow.keras.layers import Dense,Input
 
-#모델1
-input1=Input(shape=(3,))
+#덴스형 함수모델1
+input1=Input(shape=(3,)) #인풋쉐이프는 transpose후 (100,3)으로 수정했으므로
 dense1=Dense(10,activation='relu')(input1)
 dense1=Dense(5, activation='relu')(dense1)
-output1=Dense(3)(dense1)
+#output1=Dense(3)(dense1) #아웃풋값의 노드의 갯수 3
 
-#모델2
+#덴스형 함수모델2
 input2=Input(shape=(3,)) 
 dense2=Dense(10,activation='relu')(input2)
 dense2=Dense(5, activation='relu')(dense2)
 dense2=Dense(5, activation='relu')(dense2)
 dense2=Dense(5, activation='relu')(dense2)
-output2=Dense(3)(dense2)
+#output2=Dense(3)(dense2) #아웃풋값의 노드의 갯수 3
 
 #모델병합 / concatenate: 사슬같이 잇다
 from tensorflow.keras.layers import concatenate, Concatenate
@@ -49,6 +83,12 @@ from tensorflow.keras.layers import concatenate, Concatenate
 merge1=concatenate([dense1,dense2]) 
 #merge 합치다 첫번째 모델과 두번째 모델 중간중간 가중치 값 계산되서 마지막에 계산된 것 합치면 됨
 #dense1,2줄 모두 layer, middle1,output2 라인 모두 가각의 레이어이며 모델은 총 5개로 구성
+
+#모델병합시 아무것도 안넣고 걍 깔끔하게 
+# merge1 = concatenate([dense1,dense2])
+# 만 넣어줘도됨
+
+#준간층 모델구성  =>생략가능
 middle1=Dense(30)(merge1)
 middle1=Dense(10)(middle1)
 middle1=Dense(10)(middle1)
@@ -57,26 +97,25 @@ middle1=Dense(10)(middle1)
 #모델 분기1
 output1=Dense(30)(middle1)
 output1=Dense(7)(output1)
-output1=Dense(3)(output1)
+output1=Dense(3)(output1)  #아웃풋값의 노드의 갯수 3
 
 #모델 분기2
 output2=Dense(30)(middle1)
 output2=Dense(7)(output2)
 output2=Dense(7)(output2)
-output2=Dense(3)(output2)
+output2=Dense(3)(output2)  #아웃풋값의 노드의 갯수 3
 
 #모델 선언
 model=Model(inputs=[input1,input2], #2개이상은 리스트로 묶는다.[]
             outputs=[output1,output2])
 
-model.summary()
-
+#model.summary()
 
 #3.컴파일, 훈련
-model.compile(loss='mse',optimizer='adam',metrics=['mse'])
+model.compile(loss='mse',optimizer='adam',metrics=['mae'])
 model.fit([x1_train,x2_train],[y1_train,y2_train],
           epochs=10, batch_size=1,
-          validation_split=0.2, verbose=1)
+          validation_split=0.2, verbose=1)  #트레인,테스트 분리했으므로 
 
 #4.평가, 예측
 loss=model.evaluate([x1_test,x2_test],
@@ -90,53 +129,7 @@ print(loss)
 
 print("model.metrics_names:",model.metrics_names)
 #model.metrics_names: ['loss', 'dense_12_loss', 'dense_16_loss', 'dense_12_mse', 'dense_16_mse']
-'''
-model: "functional_1"
-__________________________________________________________________________________________________
-Layer (type)                    Output Shape         Param #     Connected to
-==================================================================================================
-input_2 (InputLayer)            [(None, 3)]          0
-__________________________________________________________________________________________________
-dense_2 (Dense)                 (None, 10)           40          input_2[0][0]
-__________________________________________________________________________________________________
-input_1 (InputLayer)            [(None, 3)]          0
-__________________________________________________________________________________________________
-dense_3 (Dense)                 (None, 5)            55          dense_2[0][0]
-__________________________________________________________________________________________________
-dense (Dense)                   (None, 10)           40          input_1[0][0]
-__________________________________________________________________________________________________
-dense_4 (Dense)                 (None, 5)            30          dense_3[0][0]
-__________________________________________________________________________________________________
-dense_1 (Dense)                 (None, 5)            55          dense[0][0]
-__________________________________________________________________________________________________
-dense_5 (Dense)                 (None, 5)            30          dense_4[0][0]
-__________________________________________________________________________________________________
-concatenate (Concatenate)       (None, 10)           0           dense_1[0][0]
-                                                                 dense_5[0][0]
-__________________________________________________________________________________________________
-dense_6 (Dense)                 (None, 30)           330         concatenate[0][0]
-__________________________________________________________________________________________________
-dense_7 (Dense)                 (None, 10)           310         dense_6[0][0]
-__________________________________________________________________________________________________
-dense_8 (Dense)                 (None, 10)           110         dense_7[0][0]
-__________________________________________________________________________________________________
-dense_9 (Dense)                 (None, 10)           110         dense_8[0][0]
-__________________________________________________________________________________________________
-dense_13 (Dense)                (None, 30)           330         dense_9[0][0]
-__________________________________________________________________________________________________
-dense_10 (Dense)                (None, 30)           330         dense_9[0][0]
-__________________________________________________________________________________________________
-dense_14 (Dense)                (None, 7)            217         dense_13[0][0]
-__________________________________________________________________________________________________
-dense_11 (Dense)                (None, 7)            217         dense_10[0][0]
-__________________________________________________________________________________________________
-dense_15 (Dense)                (None, 7)            56          dense_14[0][0]
-__________________________________________________________________________________________________
-dense_12 (Dense)                (None, 3)            24          dense_11[0][0]
-__________________________________________________________________________________________________
-dense_16 (Dense)                (None, 3)            24          dense_15[0][0]
-==================================================================================================
-'''
+
 y1_predict,y2_predict=model.predict([x1_test,x2_test])
 
 print("====================")
@@ -145,6 +138,51 @@ print("====================")
 print("y2_predict: \n",y2_predict)
 print("====================")
 #예측답: ytest가 (20,3)이니까 20개 나옴
+
+#실습.위 두개의 모델을 퉁칠 수 있는 RMSE와 R2값을 구하라.(위의 print(loss)=두개통합 로스값처럼)
+#=> 앙상블 모델의 RMSE,R2
+
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test,y_predict):
+    return np.sqrt(mean_squared_error(y_test,y_predict))
+#print("RMSE:",RMSE(y_test,y_predict))
+#print("mse:",mean_squared_error(y_test,y_predict))
+#print("mse:",mean_squared_error(y_p
+# redict,y_test))
+
+RMSE1=RMSE(y1_test,y1_predict)
+RMSE2=RMSE(y2_test,y2_predict)
+RMSE=(RMSE1+RMSE2)/2
+print("RMSE1:",RMSE1)
+print("RMSE2:",RMSE2)
+print("RMSE:",RMSE)
+'''
+#R2구하기
+from sklearn.metrics import r2_score
+r2=r2_score(y_test,y_predict)
+print("R2:",r2)
+
+y_pred2=model.predict(x_pred2) 
+print(y_pred2) 
+'''
+from sklearn.metrics import r2_score
+r2_1=r2_score(y1_test,y1_predict)
+r2_2=r2_score(y2_test,y2_predict)
+r2=(r2_1+r2_2)/2
+
+print("r2_1:",r2_1)
+print("r2_2:",r2_2)
+print("r2:",r2)
+
+'''
+RMSE1: 45.34097095121749
+RMSE2: 41.885130911451846
+RMSE: 43.61305093133467
+r2_1: -60.82868110674131
+r2_2: -51.76283282614914
+r2: -56.295756966445225
+'''
+
 '''
 y1_predict:
  [[845.501     83.44869  271.89594 ]
@@ -189,48 +227,4 @@ y2_predict:
  [650.0011   900.6825    43.33221 ]
  [652.0464   903.4505    43.493908]
  [654.09174  906.2185    43.655693]]
-'''
-
-#실습.위 두개의 모델을 퉁칠 수 있는 RMSE와 R2값을 구하라.(위의 print(loss)=두개통합 로스값처럼)
-#=> 앙상블 모델의 RMSE,R2
-
-from sklearn.metrics import mean_squared_error
-def RMSE(y_test,y_predict):
-    return np.sqrt(mean_squared_error(y_test,y_predict))
-#print("RMSE:",RMSE(y_test,y_predict))
-#print("mse:",mean_squared_error(y_test,y_predict))
-#print("mse:",mean_squared_error(y_p
-# redict,y_test))
-
-RMSE1=RMSE(y1_test,y1_predict)
-RMSE2=RMSE(y2_test,y2_predict)
-RMSE=(RMSE1+RMSE2)/2
-print("RMSE1:",RMSE1)
-print("RMSE2:",RMSE2)
-print("RMSE:",RMSE)
-'''
-#R2구하기
-from sklearn.metrics import r2_score
-r2=r2_score(y_test,y_predict)
-print("R2:",r2)
-
-y_pred2=model.predict(x_pred2) 
-print(y_pred2) 
-'''
-from sklearn.metrics import r2_score
-r2_1=r2_score(y1_test,y1_predict)
-r2_2=r2_score(y2_test,y2_predict)
-r2=(r2_1+r2_2)/2
-
-print("r2_1:",r2_1)
-print("r2_2:",r2_2)
-print("r2:",r2)
-
-'''
-RMSE1: 45.34097095121749
-RMSE2: 41.885130911451846
-RMSE: 43.61305093133467
-r2_1: -60.82868110674131
-r2_2: -51.76283282614914
-r2: -56.295756966445225
 '''
