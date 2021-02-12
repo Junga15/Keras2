1a7ec8db8ce56e5fd0fde4ac95eda764b9de2fb9
JungaKim <jungakim1551@gmail.com>
2021-02-12 19:13:22 +0900

    2021-02-12

---
+++ keras20_boston_keras1.py
@@ -0,0 +1,9 @@
+#보스턴 하우징 텐서플로우로 
+#텐서플로우는 사이킷런과 다른 방식으로 데이터를 땡겨옴, 찾아야함
+#2개의 파일을 만드시오. keras20_1,2
+#1.Earlystopping을 적용하지 않은 최고의 모델
+
+
+from tensorflow.keras.datasets import boston_housing
+
+#이걸로 만들어라!!
